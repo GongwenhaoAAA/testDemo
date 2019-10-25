f5301d525fccd052b875b22d80db1322589aeda1
gwh <335028495@qq.com>
2019-10-26 03:40:46 +0800

    456

--- src/main/java/com/ali/demo/controller/HelloController.java
+++ src/main/java/com/ali/demo/controller/HelloController.java
@@ -3,24 +3,30 @@ package com.ali.demo.controller;
 import com.ali.demo.Pojo.People;
 import com.ali.demo.mapper.PeopleMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/hello")
 public class HelloController {
     @Autowired
     PeopleMapper peopleMapper;
 
     @RequestMapping(value = "/test",method = RequestMethod.GET)
-    @ResponseBody
     public List<People> test(){
         List<People> select = peopleMapper.select();
 
         return select;
+    }
+
+    @RequestMapping("/test2")
+    public String test2(){
+        String str = "what your name?";
+        String str1 = "my name is liHua";
+        return str+" "+str1;
     }
 }
